314023ebdd6be50ff82b3f1286977e7e61dcc93e
Jovian Lim <kiriketsuki@gmail.com>
2022-07-27 18:36:24 +0800

    fresh start

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-import { createApp } from 'vue';
-import { TroisJSVuePlugin  } from 'troisjs';
-import App from './App.vue';
-import router from './router';
-import './index.css';
-import 'tw-elements';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-createApp(App)
-    .use(router)
-    .use(TroisJSVuePlugin)
-    .mount('#app')
+createApp(App).mount('#app')

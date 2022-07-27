a88d574d17e32136598ae2dfcf76fb27c0036d65
Jovian Lim <kiriketsuki@gmail.com>
2022-07-27 18:55:54 +0800

    threejs beginning

--- src/three.js
+++ src/three.js
@@ -1,11 +1,23 @@
 import * as THREE from 'three';
 
+//? Set up
 const canvas = document.getElementById('webgl');
-console.log(canvas)
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+//? Renderer options
 const renderer = new THREE.WebGLRenderer({ canvas });
+
+//? Camera
+camera.position.set(0, 0, 5);
+
+//? Models
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+
+
+
+//? Scene
 
 function loop() {
     requestAnimationFrame(loop);
